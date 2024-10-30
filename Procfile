50caa544b672a74d4938e3aa253db176153ab4c4
Mskerz <mskz_wichasin@kkumail.com>
2024-10-31 01:58:45 +0700

    add docker deploy --

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker --host=0.0.0.0 --port=${PORT}
--- config/db_connect.py
+++ config/db_connect.py
@@ -2,11 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,Session
 from urllib.parse import quote
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # MySQL connection settings
 # SQLALCHEMY_DATABASE_URL = "mysql://root:1234@db:3306/thesis_base"
-
-SQLALCHEMY_DATABASE_URL = "mysql://admin:Admin1234@34.142.236.17:3306/thesis_base"
+username = os.environ.get("MYSQL_USER")
+password = os.environ.get("MYSQL_PASSWORD")
+SQLALCHEMY_DATABASE_URL = f"mysql://{username}:{password}@34.142.236.17:3306/thesis_base"
  
 #  Create SQLAlchemy engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)  
